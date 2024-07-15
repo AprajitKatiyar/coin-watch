ce22e0bdb005e80d48aa2130c833f92a8e4ff54f
Aprajit Katiyar <aprajit.katiyar@siemens-healthineers.com>
2024-07-15 23:20:27 +0530

    Adding control to select the crypto

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,6 +4,9 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const items = useSelector((state: any) => state);
   const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(false);
+  const [selectedCrypto, setSelectedCrypto] = useState("");
+  const cryptos = ["BTC", "ETH", "GRIN"];
   const [socket, setSocket] = useState<WebSocket | null>(null);
   useEffect(() => {
     const newSocket = new WebSocket("ws://localhost:3000");
@@ -18,8 +21,26 @@ export default function Home() {
     return () => newSocket.close();
   }, []);
   console.log(items.cryptoData);
+  const handleSelectChange = (event) => {
+    setSelectedCrypto(event.target.value);
+    console.log(event.target.value);
+  };
   return (
-    <div className="h-screen w-full flex items-center justify-center p-10">
+    <div className="h-screen w-full flex-col items-center justify-center p-10 relative">
+      <select
+        id="site"
+        className="p-2 bg-[#E8E8E8] w-[50] my-2 focus:outline-none focus:shadow-md border shadow-md border-gray-300 text-gray-900 text-sm"
+        value={selectedCrypto}
+        onChange={handleSelectChange}
+      >
+        <option value="">Select a site</option>
+        {cryptos.map((crypto, index) => (
+          <option key={index} value={crypto}>
+            {crypto}
+          </option>
+        ))}
+      </select>
+
       <div className="max-h-96 overflow-y-auto w-full">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 shadow-lg border rounded-sm flex-1">
           <caption className="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900  bg-gray-200"></caption>
@@ -35,7 +56,7 @@ export default function Home() {
           </thead>
           <tbody>
             {items.cryptoData
-              .filter((data: any) => data.code === "ETH")
+              .filter((data: any) => data.code === selectedCrypto)
               .map((data, index) => (
                 <tr key={index} className="bg-white border-b  cursor-pointer">
                   <th
